fbff20ab9a2ef310d5a3baeb8d5944400f0087f6
Pavel <53278378+smoloff@users.noreply.github.com>
2024-02-20 18:22:06 +0200

    todo: слушатель которій клике переключает select на "Власний розмір"

--- script.js
+++ script.js
@@ -8,33 +8,39 @@ const dpiSelect = document.getElementById('dpi_values');
 let dpi = 300;
 let result = {};
 
-const convetFunc = () => {
+const convertFunc = () => {
     result.widthPixel = Math.ceil(inputWidth.value / 2.54 * dpi);
     result.heightPixel = Math.ceil(inputHeight.value / 2.54 * dpi);
     resultCnt.innerHTML = `${result.widthPixel} x ${result.heightPixel} px`
 }
+// мделать слушатель которій при клике ппеерключает html елемент select на "Власний розмір"
+// inputWidth.addEventListener('input', ()=> {
+
+// });
 
 selectSize.addEventListener('change', (e) => {
     let w = selectSize.value.split('x');
-    inputWidth.value = Number(w[0]) / 10;
-    inputHeight.value = Number(w[1]) / 10;
-    convetFunc();
+    inputWidth.value = Number(w[0]) / 10; //перевод мм в см
+    inputHeight.value = Number(w[1]) / 10; //перевод мм в см
+    convertFunc();
 });
 
 dpiSelect.addEventListener('change', (e) => {
     let i = dpiSelect.selectedIndex
-    console.log(dpiSelect[i].title)
+    dpi = dpiSelect.value;
+    let description = dpiSelect[i].title  // описання для dpi
+    convertFunc();
 });
 
 
 
 
-convert.addEventListener('click', convetFunc)
+convert.addEventListener('click', convertFunc)
 rotate.addEventListener('click', (e) => {
     let buffer = inputWidth.value;
     inputWidth.value = inputHeight.value;
     inputHeight.value = buffer;
-    convetFunc();
+    convertFunc();
 });
 
 
